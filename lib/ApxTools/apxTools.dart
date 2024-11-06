dab6a05f9a17cbdb353a20567e13a2b7b42bb2f3
Nakul Dev <nakuldev1561@gmail.com>
2024-11-06 22:31:24 +0530

    "Updated main.dart to import ApxTools and use apxTools() as home, replaced favicon and index.html with new versions, and added loading animation to index.html"

---
+++ lib/ApxTools/apxTools.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class apxTools extends StatelessWidget {
+  const apxTools({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Website Coming Soon!',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue, // You can change this color as needed
+          ),
+        ),
+      ),
+    );
+  }
+}
